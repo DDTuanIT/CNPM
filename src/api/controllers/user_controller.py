50960e3b8bac31d5484a6b4962e9c12e9d27e87d
NewbieMt-std <tuanma2175@ut.edu.vn>
2025-08-13 03:59:39 +0000

    Đổi phương thức login; kiểm tra repo; sửa một số import

--- src/api/controllers/user_controller.py
+++ src/api/controllers/user_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from infrastructure.databases.session import get_db_session 
+from infrastructure.databases.mssql import get_db_session 
 from infrastructure.repositories.user_repository import UserRepository
 from services.user_service import UserService
 from api.schemas.user_schema import RegisterSchema, LoginSchema
@@ -19,29 +19,15 @@ def register():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-#@auth_bp.route("/login", methods=["POST"])
-#def login():
-#    try:
-#        data = LoginSchema().load(request.json)
-#        db = get_db_session()
-#        user_repo = UserRepository(db)
-#        service = UserService(user_repo)
-
-#        result = service.login(data["email"], data["password"])
-#        return jsonify({"message": "Login successful", "user": result})
-#    except Exception as e:
-#        return jsonify({"error": str(e)}), 401
-@auth_bp.route("/login", methods=["GET"])
+@auth_bp.route("/login", methods=["POST"])
 def login():
     try:
-        email = request.args.get("email")
-        password = request.args.get("password")
-
+        data = LoginSchema().load(request.json)
         db = get_db_session()
         user_repo = UserRepository(db)
         service = UserService(user_repo)
 
-        result = service.login(email, password)
+        result = service.login(data["email"], data["password"])
         return jsonify({"message": "Login successful", "user": result})
     except Exception as e:
         return jsonify({"error": str(e)}), 401
